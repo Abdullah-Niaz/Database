d7698cd290cb6c889f52f78beeb1e2fa00a8a3de
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-01-13 19:14:51 +0500

    Between Operator

---
+++ 02. Reteriving Data from single DB/between_operator.sql
@@ -0,0 +1,6 @@
+
+-- The Between Operator
+-- better and concise way of getting values
+select * from customers where points between 1000 and 3000;
+select * from customers where birth_date between '1990-01-01' and '2000-01-01';
+
