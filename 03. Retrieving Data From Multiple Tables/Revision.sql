b71f9584aa365adb555709257b7ad112f58e110e
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-01-14 20:21:07 +0500

    Join Multiple tables

--- 03. Retrieving Data From Multiple Tables/Revision.sql
+++ 03. Retrieving Data From Multiple Tables/Revision.sql
@@ -1,3 +1,10 @@
+-- Join statement 
+-- suppose that you're working on ecommerce website & now on position where you want to combine data of two table
+	-- like order_details 
+    -- customer details 
+    -- use the multiple table joining method
+    -- there is also other method which is called implicit method call 
+    -- mentioned one is explicit call 
 select o.order_id,
 	c.first_name,
 	c.customer_id,
@@ -5,3 +12,8 @@ select o.order_id,
 from orders o 
 join customers c
 	on o.customer_id = c.customer_id;
+    
+select *
+from order_items oi 
+join products p
+	on oi.product_id = p.product_id;
