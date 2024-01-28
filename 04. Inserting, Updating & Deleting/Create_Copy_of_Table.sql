e089b7e998697bb1568973dbbb1d40198363d4a7
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-01-28 18:58:16 +0500

    Copy of multiple table by joining

--- 04. Inserting, Updating & Deleting/Create_Copy_of_Table.sql
+++ 04. Inserting, Updating & Deleting/Create_Copy_of_Table.sql
@@ -12,3 +12,21 @@ select * from orders
 where order_date > '2000-05-06';
 
 select * from orders_archieved;
+
+use sql_invoicing;
+
+CREATE TABLE invoice_archived as
+select 
+	i.invoice_id,
+    i.number,
+    c.name as client,
+    i.invoice_total,
+    i.payment_total,
+    i.due_date,
+    i.payment_date
+from invoices i 
+join clients c
+	using (client_id)
+where payment_date is not null;    
+
+select * from invoice_archived;
